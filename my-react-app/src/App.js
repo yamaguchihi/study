dd9ec9bb367f0c3e243b939514c8236aec86b5b4
yamaguchihi <hinatayamaguchi04@gmail.com>
2024-07-02 16:17:43 +0900

    初めてのコミット

---
+++ my-react-app/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function  App () {
+  return (
+    <div className = Title >
+      <header>My Work</header>
+      <div className = lefttitle>
+        <li>About</li>
+        <li>Works</li>
+        <li>News</li>
+        <li>Contact</li>
+        <li>instagram</li>
+      </div>
+    </div>
+  )
+}
+
+function app
+
+export default App;
